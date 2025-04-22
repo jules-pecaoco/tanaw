0f3039869b89b14173728a78511205fe6c414d38
jules <julesalfonzp@gmail.com>
2025-04-22 23:39:45 +0800

    update .gitignore and added features: Analyze Capture Image Using AI, added subtype, slight refresh, and refactor

--- app/(tabs)/radar.jsx
+++
@@ -1,7 +0,0 @@
-import RadarScreen from "@/views/Radar/RadarScreen";
-
-const radar = () => {
-  return <RadarScreen />;
-};
-
-export default radar;
--- constants/index.js
+++
@@ -1,5 +0,0 @@
-import icons from './visuals/icons';
-import images from './visuals/images';
-import sources from './visuals/sources';
-
-export { icons, images, sources };
